75b934b1edbb860c5af4aced83ccae27e4302103
Josse Carballo <josse@pulumi.com>
2024-05-20 13:14:54 -0400

    fixes typo with git dir and url

--- aws/python/ecs-flask-app/deployments.py
+++ aws/python/ecs-flask-app/deployments.py
@@ -6,8 +6,8 @@ deployment_settings_resource = service.DeploymentSettings("deploymentSettingsRes
     source_context=service.DeploymentSettingsSourceContextArgs(
         git=service.DeploymentSettingsGitSourceArgs(
             branch="main",
-            repo_dir="https://github.com/notjosse/pulumi-home.git",
-            repo_url="aws/python/ecs-flask-app",
+            repo_url="https://github.com/notjosse/pulumi-home.git",
+            repo_dir="aws/python/ecs-flask-app",
         ),
     ),
     stack="dev",
