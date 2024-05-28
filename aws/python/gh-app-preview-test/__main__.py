e183866756359b7a2f62ed9adcea76bcbdc567fe
Josse Carballo <josse@pulumi.com>
2024-05-28 13:31:40 -0400

    changes name to trigger the pulumi gh app

--- aws/python/gh-app-preview-test/__main__.py
+++ aws/python/gh-app-preview-test/__main__.py
@@ -4,7 +4,7 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket-josse-2')
+bucket = s3.Bucket('my-bucket-josse-3')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
