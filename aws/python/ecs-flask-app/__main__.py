2c6154aea5ee7f9de4a62755b3e1668c3cca3c20
Josse Carballo <josse@pulumi.com>
2024-06-04 10:45:44 -0400

    adds prefix for resources and turns of deployments import

--- aws/python/ecs-flask-app/__main__.py
+++ aws/python/ecs-flask-app/__main__.py
@@ -2,39 +2,42 @@ from pulumi import Config, Output, export
 import pulumi_aws as aws
 import pulumi_awsx as awsx
 
-from deployments import deployment_settings_resource
+# from deployments import deployment_settings_resource
 
 config = Config()
 container_port = config.get_int("containerPort", 80)
 cpu = config.get_int("cpu", 512)
 memory = config.get_int("memory", 128)
 
+# prefix
+name = 'josse'
+
 # An ECS cluster to deploy into
-cluster = aws.ecs.Cluster("cluster")
+cluster = aws.ecs.Cluster(f"{name}-cluster")
 
 # An ALB to serve the container endpoint to the internet
-loadbalancer = awsx.lb.ApplicationLoadBalancer("loadbalancer")
+loadbalancer = awsx.lb.ApplicationLoadBalancer(f"{name}-loadbalancer")
 
 # An ECR repository to store our application's container image
-repo = awsx.ecr.Repository("repo", awsx.ecr.RepositoryArgs(
+repo = awsx.ecr.Repository(f"{name}-repo", awsx.ecr.RepositoryArgs(
     force_delete=True,
 ))
 
 # Build and publish our application's container image from ./app to the ECR repository
 image = awsx.ecr.Image(
-    "image",
+    f"{name}-image",
     repository_url=repo.url,
     context="./frontend",
     platform="linux/amd64")
 
 # Deploy an ECS Service on Fargate to host the application container
 service = awsx.ecs.FargateService(
-    "josse-flask-service",
+    f"{name}-flask-service",
     cluster=cluster.arn,
     assign_public_ip=True,
     task_definition_args=awsx.ecs.FargateServiceTaskDefinitionArgs(
         container=awsx.ecs.TaskDefinitionContainerDefinitionArgs(
-            name="flask-app",
+            name=f"{name}-flask-app",
             image=image.image_uri,
             cpu=cpu,
             memory=memory,
